920d986a3e7fe31d566dd230ecb97392289b96f3
eriks-briedis <eriks.briedis@gmail.com>
2013-05-10 16:51:25 +0200

    v.0.1.1
    added control buttons
    updated animate function
    and other improvements

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 /*
- * Ajax Slider v.0.1
+ * Ajax Slider v.0.1.1
  *
  * Author Eriks Briedis
  * http://eriks.designschemers.com
@@ -28,57 +28,79 @@
 		var wrapper = this,
 			slider = $(wrapper).selector,
 			slides = $(wrapper).children('li'),
-			count = 0,
-			num_slides = $(slides).length,
 			cur_slide = 0;
 
-		$(slides).css('display', 'none');
+		//let's find the index for the next slide we need
+		// @direction = undefined, right or left
+		// @t_slide = int. if defined jump to that slide number
+		var get_slide = function(direction, t_slide){
+			if(t_slide != undefined){
+				cur_slide = t_slide;
+			}else if(direction == undefined || direction == 'right'){
+				// checking if we need to return to the first slide or continue
+				if(cur_slide + 1 >= $(slides).length){
+					cur_slide = 0;
+				}else{
+					cur_slide++;
+				}
+			}else{
+				// checking if this is the first slide
+				// if this is the first slide then go to the last
+				if(cur_slide - 1 < 0){
+					cur_slide = $(slides).length - 1;
+				}else{
+					cur_slide--;
+				}
+			}
+		};
 
 		// let's get the animation going
-		var animate = function(direction){
+		var animate = function(){
 
-			//check if current slide count is in the range of total slide number
-			if(count < num_slides){
+			$(slides).fadeOut(defaults.effect_duration)
+			$(slides).eq(cur_slide).fadeIn(defaults.effect_duration);
+			create_ctrls();
 
-				if(direction == undefined || direction == 'right'){
-					count++;
-				}else{
-					count = cur_slide - 1;
-				}
+		}
 
-				$(slides).fadeOut(defaults.effect_duration);
-				$(slides).eq(count).fadeIn(defaults.effect_duration, function(){
-
-					if( defaults.autoplay === true ){ // only execute if autoplay is enabled
-						setTimeout(function(){
-						}, defaults.slide_duration); //let's wait for slide_duration and run animate again to show the next slide
-					}
-					cur_slide = $(this).index();
-				});
-
-			}else{
-				count = 0; // reset count to start over
-				animate(); // starting over
-
+		var create_ctrls = function(){
+			if($('#aslider-ctrls') != 0){ //let's remove all children for aslider-ctrls to update the active one
+				$('#aslider-ctrls').children().remove();
 			}
-
+			$(slider).append('<span id="aslider-ctrls"></span>');
+			for(var i = 0; $(slides).length > i; i++){
+				if(i == cur_slide){
+					$('#aslider-ctrls').append('<span data-slide="'+i+'" class="ctrl-btn ctrl-active"></span>');
+				}else{
+					$('#aslider-ctrls').append('<span data-slide="'+i+'" class="ctrl-btn"></span>');
+				}
+			}
+			$('.ctrl-btn').click(function(){
+				var t_slide = $(this).context.getAttribute('data-slide');
+				get_slide(false, t_slide);
+				animate();
+			});
 		}
 
 		var init = function(){
 
-			// adding classes to ul an li tags
+			// adding classes to ul an li tags and hiding the whole thing
+			$(slides).css('display', 'none');
 			$(slider).addClass('aslider').children('li').addClass('slide');
 			$(slider).css({'width' : defaults.width, 'height' : defaults.height});
 
 
-
 			// adding prev and next controls
-			$(slider).append('<a class="slide-controls slide-left" data-direction="left" href="#"></a>');
-			$(slider).append('<a class="slide-controls slide-right" data-direction="right" href="#"></a>');
+			$(slider).append('<span class="slide-controls slide-left" data-direction="left"></span>');
+			$(slider).append('<span class="slide-controls slide-right" data-direction="right"></span>');
+
+			// adding controls
+			create_ctrls();
 
 			$('.slide-controls').click(function(){
 				var direction = $(this).context.getAttribute('data-direction');
-				animate(direction);
+				get_slide(direction);
+				animate();
 			});
 
 			animate();
